134121fef0aeb2f39d0b37a6a17920ed5105b237
Enix Yu <enix223@163.com>
2015-11-12 20:04:15 +0800

    downgrade the django version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4',
+        'Django==1.6',
 	'beautifulsoup4==4.4.1',
 	'evernote==1.25.1',
 	'html2text==2015.11.4',
